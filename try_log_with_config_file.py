70203873987bdb357aa4e83610e1c8783915d1da
Artem <you@example.com>
2022-10-19 13:39:01 +0300

    Add changes to name methods and name modules

--- try_log_with_config_file.py
+++ try_log_with_config_file.py
@@ -19,9 +19,9 @@ def logging_1():
     logger = logging.getLogger("Mine")
     logger.info('This is a debug message 1')
 
-def logging_2():
+def logging_2(name_log):
     now = datetime.now()
-    name_file_log = str(now.strftime("%d %b %Y - %H %M %S"))+" test_log2.txt"
+    name_file_log = str(now.strftime("%d %b %Y - %H %M %S"))+f" {name_log}.txt"
 
     logging.config.fileConfig(fname='config.ini',
                               defaults={'logfilename': (os.path.join(os.path.dirname(os.path.realpath(__file__)), 'logs_files', name_file_log))},
@@ -30,5 +30,6 @@ def logging_2():
     logger = logging.getLogger("Mine")
     logger.info('This is a debug message 2')
 
-l1 = logging_1()
-# l2 = logging_2()
+# l1 = logging_1()
+l2 = logging_2("dog")
+l3 = logging_2("cat")
